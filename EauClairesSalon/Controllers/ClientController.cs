81c6ff9bb00885c97a1aee77352591f9a13de096
ellehailu <hailu.elle@gmail.com>
2023-07-28 13:26:14 -0700

    Local database created with two tables for stylists and clients.

--- EauClairesSalon/Controllers/ClientController.cs
+++ EauClairesSalon/Controllers/ClientController.cs
@@ -1,7 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using EauClairesSalon.Models;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace EauClairesSalon.Controllers{
-    public class ClientController : Controller{
+namespace EauClairesSalon.Controllers
+{
+    public class ClientController : Controller
+    {
 
+        private readonly EauClairesSalonContext _db;
+
+        public ClientController(EauClairesSalonContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Client> model = _db.Clients.ToList();
+            return View(model);
+        }
     }
 }
