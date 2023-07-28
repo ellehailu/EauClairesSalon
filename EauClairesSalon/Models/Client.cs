bb9377ea1564b49ecff0584cda1f8b94239d2ca2
ellehailu <hailu.elle@gmail.com>
2023-07-28 10:09:08 -0700

    complete EauClairesSalonContext file

---
+++ EauClairesSalon/Models/Client.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace EauClairesSalon.Models
+{
+    public class Client
+    {
+
+    }
+}
--- EauClairesSalon/Models/EauClairesSalonContext.cs
+++ EauClairesSalon/Models/EauClairesSalonContext.cs
@@ -1,3 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace EauClairesSalon.Models;
+namespace EauClairesSalon.Models
+{
+    public class EauClairesSalonContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public EauClairesSalonContext(DbContextOptions options) : base(options) { }
+    }
+}
