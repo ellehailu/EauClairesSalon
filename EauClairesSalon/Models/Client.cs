672db35f042f6d997725fbd64e1328fa59b8c6f5
ellehailu <hailu.elle@gmail.com>
2023-07-28 13:30:44 -0700

    add create methods in client controller

--- EauClairesSalon/Models/Client.cs
+++ EauClairesSalon/Models/Client.cs
@@ -5,6 +5,10 @@ namespace EauClairesSalon.Models
 {
     public class Client
     {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
 
+        //public StylistId { get; setl }
+        
     }
 }
