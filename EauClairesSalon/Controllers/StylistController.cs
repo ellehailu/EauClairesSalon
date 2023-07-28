e262fc2ce6e8fc93fa239489c034913278bf0552
ellehailu <hailu.elle@gmail.com>
2023-07-28 14:43:05 -0700

    updated Stylist controller to include route for index

--- EauClairesSalon/Controllers/StylistController.cs
+++ EauClairesSalon/Controllers/StylistController.cs
@@ -1,5 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace EauClairesSalon.Controllers{
-    public class StylistController : Controller{}
+namespace EauClairesSalon.Controllers
+{
+    public class StylistController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
 }
--- EauClairesSalon/Models/Stylist.cs
+++ EauClairesSalon/Models/Stylist.cs
@@ -2,3 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace EauClairesSalon.Models;
+
+public class Stylist
+{
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Speciality { get; set; }
+
+    //connect clients
+    //public Client client { get; set; }
+}
